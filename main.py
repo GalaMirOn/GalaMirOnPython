c60da2ecc03dae6c655398a8f43f51d05d157c93
GalaMirOn <mironoff0408@gmail.com>
2024-08-28 16:01:36 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1-st program
+print('№1 ', 9**0.5*5)
+
+# 2-nd program
+print('№2 ', 9.99 > 9.98 and 1000 != 1000.1)
+
+# 3-rd program
+print ('№3 ', 2*2+2, 2*(2+2),2*2+2==2*(2+2))
+
+# 4-th program
+print('№4 ',int(float('123.456')*10)%10)
