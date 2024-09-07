5a16079deb1d64dc830dcd4d7b5e324b1a42df4c
GalaMirOn <mironoff0408@gmail.com>
2024-09-07 13:57:24 +0400

    Add files via upload

--- module_2_3_new.py
+++ module_2_3_new.py
@@ -4,8 +4,7 @@ while i < len(my_list):
     if my_list[i] < 0:
         break
     elif my_list[i] == 0:
-        i +=1
+        i += 1
         continue
-    else:
-        print(my_list[i])
+    print(my_list[i])
     i +=1
