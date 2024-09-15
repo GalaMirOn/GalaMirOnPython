6cb20484754a6123c48d7ede94daef4dee8d1544
GalaMirOn <mironoff0408@gmail.com>
2024-09-15 11:06:53 +0400

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,15 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+
+test_function()
+# inner_function()
+"""
+File "C:\PycharmProjects\PythonMirOn\Gala_Py\module_4_2.py", line 9, in <module>
+    inner_function()
+    ^^^^^^^^^^^^^^
+NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
+"""
