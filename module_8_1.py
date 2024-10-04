b0da1b09cd418dc1e524865bf3a21a52fac38d06
GalaMirOn <mironoff0408@gmail.com>
2024-10-04 12:27:20 +0400

    Add files via upload

--- module_8_1.py
+++ module_8_1.py
@@ -1,8 +1,9 @@
 def add_everything_up(a, b):
     try:
         sum = a + b
-    except TypeError:
+    except TypeError as exp:
         # print('Не мешайте кашу с мухами! Ладно, одну муху можете выкинуть из каши!')
+        print('Ошибка сложения строки и числа:', exp)
         if type(a) != 'str':
             a = str(a)
         if type(b) != 'str':
