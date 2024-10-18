8e9a6ab41b8597fdaaa4d4aa6faa509cbed0d546
GalaMirOn <mironoff0408@gmail.com>
2024-10-18 14:06:56 +0400

    Add files via upload

--- suite_12_3.py
+++ suite_12_3.py
@@ -2,7 +2,6 @@ import unittest
 import tests_12_3
 
 
-
 zabegST = unittest.TestSuite()
 zabegST.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.RunnerTest))
 zabegST.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.TournamentTest))
