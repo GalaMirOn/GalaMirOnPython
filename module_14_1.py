e649af82ced131e355a09f0639ef431ada07d452
GalaMirOn <mironoff0408@gmail.com>
2024-10-29 11:25:49 +0400

    Add files via upload

--- module_14_1.py
+++ module_14_1.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect("not_telegram.db")
+connection = sqlite3.connect("not_telegram1.db")
 cursor = connection.cursor()
 
 cursor.execute('''
@@ -13,14 +13,14 @@ balance INTEGER NOT NULL
 )
 ''')
 
-# for i in range(1,11):
-#     cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES(?,?,?,?)", (f"User{i}", f"exampl{i}@gmail.com", i*10, 1000))
-# connection.commit()
-#cursor.execute("CREATE INDEX IF NOT EXISTS idx_balance ON Users (balance)")
-#cursor.execute("UPDATE Users SET balance=? WHERE id % 2 =?", (500, 1))
-#connection.commit()
-#cursor.execute("DELETE FROM Users WHERE id % 3 =?", (1, ))
-#connection.commit()
+for i in range(1,11):
+    cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES(?,?,?,?)", (f"User{i}", f"exampl{i}@gmail.com", i*10, 1000))
+connection.commit()
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_balance ON Users (balance)")
+cursor.execute("UPDATE Users SET balance=? WHERE id % 2 =?", (500, 1))
+connection.commit()
+cursor.execute("DELETE FROM Users WHERE id % 3 =?", (1, ))
+connection.commit()
 cursor.execute("CREATE INDEX IF NOT EXISTS idx_age ON Users (age)")
 cursor.execute("SELECT username, email, age, balance FROM Users WHERE age!= ?", (60, ))
 users = cursor.fetchall()
