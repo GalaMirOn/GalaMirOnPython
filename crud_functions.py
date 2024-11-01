8a42d0585bc7e01e70539975183bca686b2e332c
GalaMirOn <mironoff0408@gmail.com>
2024-11-01 12:48:09 +0400

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -30,7 +30,40 @@ def get_all_products():
     connection.close()
     return products
 
+def init_user_db():
+    connection = sqlite3.connect("users.db")
+    cursor = connection.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    )
+    ''')
+
+    connection.commit()
+    connection.close()
+
+def add_user(username, email, age):
+    #print(username, email, age)
+    connection = sqlite3.connect("users.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES(?,?,?,?)", (username, email, age, 1000))
+    connection.commit()
+    connection.close()
+
+def is_included(username):
+    connection = sqlite3.connect("users.db")
+    cursor = connection.cursor()
+    check_user = cursor.execute(f"SELECT * FROM Users WHERE username=?", (username, ))
+    user_have = False if check_user.fetchone()==None else True
+    connection.close()
+    return user_have
 
 if __name__ == '__main__':
     # initiate_db()
-    get_all_products()
+    #init_user_db()
+    # get_all_products()
+    pass
